fe672d1e4d5f48369c060a73b324f4ea3736ff38
Benjamin DUBUS <benjamin.dubus@epitech.eu>
2018-11-20 15:01:09 -0500

    User Class

---
+++ src/java/FindTheSmile/Users.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FindTheSmile;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bdubus
+ */
+public class Users {
+    private String name;
+    private String surname;
+    private Date birstDate;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private Integer lastScore;
+    private Integer bestScore;
+
+    public Users(String argName, String argSurname, Date argBirstDate,
+            String argPhoneNumber, String argEmail, String argPassword,
+            Integer argLastScore, Integer argBestScore) {
+        this.name = argName;
+        this.surname = argSurname;
+        this.birstDate = argBirstDate;
+        this.phoneNumber = argPhoneNumber;
+        this.email = argEmail;
+        this.password = argPassword;
+        this.lastScore = argLastScore;
+        this.bestScore = argBestScore;
+    }
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Date getBirstDate() {
+        return birstDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getLastScore() {
+        return lastScore;
+    }
+
+    public Integer getBestScore() {
+        return bestScore;
+    }
+}
